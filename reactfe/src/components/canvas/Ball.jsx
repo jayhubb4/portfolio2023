c64e3792244ad4d1703ff639d4649178fe04e560
Jonathan Hubbard <gsusplaya@gmail.com>
2023-04-06 23:12:02 -0400

    front end finished

--- reactfe/src/components/canvas/Ball.jsx
+++ reactfe/src/components/canvas/Ball.jsx
@@ -7,33 +7,28 @@ const Ball = (props) => {
   const [decal] = useTexture([props.imgUrl])
 
   return (
-    <Float 
-      speed={1.75} 
-      rotationIntensity={1}
-      floatIntensity={1}
-    >
+    <Float speed={5} rotationIntensity={1} floatIntensity={5}>
       <ambientLight intensity={0.25} />
       <directionalLight position={[0, 0, 0.05]} />
       <mesh castShadow receiveShadow scale={2.75}>
         <icosahedronGeometry args={[1, 1]} />
-        <meshStandardMaterial 
+        <meshStandardMaterial
           color='#fff8eb'
           polygonOffset
           polygonOffsetFactor={-5}
           flatShading
         />
         <Decal
-          map={decal}
           position={[0, 0, 1]}
           rotation={[2 * Math.PI, 0, 6.25]}
-          flatShading
           scale={1}
+          map={decal}
+          flatShading
         />
       </mesh>
     </Float>
-  )
-}
-
+  );
+};
 const BallCanvas = ({ icon }) => {
   return (
     <Canvas 
