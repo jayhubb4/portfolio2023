d2af86fce742fd28cbc82aace61054e1a1052ef1
Jonathan Hubbard <gsusplaya@gmail.com>
2023-04-18 13:21:58 -0400

    env changes

--- reactfe/src/utils/client.js
+++ reactfe/src/utils/client.js
@@ -2,15 +2,13 @@ import React from 'react';
 import { createClient } from '@sanity/client';
 import ImageUrlBuilder from '@sanity/image-url';
 
-const projectId = import.meta.VITE_APP_SANITY_ID;
-const token = import.meta.VITE_APP_SANITY_TOKEN;
 
 export const client = createClient({
     projectId: 'api8ljjp',
     dataset: 'production',
     apiVersion: '2022-02-01',
     useCdn: true,
-    token: import.meta.VITE_APP_SANITY_TOKEN,
+    token: import.meta.env.VITE_APP_SANITY_TOKEN,
 })
 
 
